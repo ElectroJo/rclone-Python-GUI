581554762462400a1ba3a579b162253ae21ecd24
ElectroJo <ElectroJo@users.noreply.github.com>
2017-09-14 14:44:30 -0400

    Tkinkter and config file stuff
    Moved the console window to the class so mutiple can open for mutiple programs.
    
    added the ability to load default config files and custom config files

--- script.py
+++ script.py
@@ -3,29 +3,44 @@ from subprocess import Popen, PIPE, STDOUT
 import tkinter
 from threading import Thread
 import configparser
+import getpass
+from tkinter.filedialog import askopenfilename
 
 GUI = tkinter.Tk()
 GUI.minsize(width=200, height=200)
-ConsoleGUIWin = tkinter.Toplevel()
-ConsoleGUIWin.title("Console")
-ConsoleGUIWin.deiconify()
+ConfigButtons = []
+
 
 
 #Some code from: https://gist.github.com/zed/42324397516310c86288
 class rcloneProcess:
-    def __init__(self, GUI, Console):
+    def __init__(self, GUI):
         self.GUI = GUI
-        self.Console = Console
         self.process = Popen([r"rclone\rclone-v1.37-windows-amd64\rclone.exe", "ls","secretC:"], stdout=PIPE, stderr=STDOUT)
         t = Thread(target=self.reader_thread, args=[]).start()
         self.EndButton = tkinter.Button(GUI, text="Stop subprocess")
         self.EndButton = tkinter.Button(GUI, text="Stop subprocess", command = lambda: self.stop(self.EndButton))
         self.EndButton.pack()
+        self.ConsoleGUIWin = tkinter.Toplevel()
+        self.ConsoleGUIWin.title("Console")
+        self.ConsoleFrame = tkinter.Frame(self.ConsoleGUIWin, width=900, height=500)
+        self.ConsoleFrame.pack(fill="both", expand=True)
+        self.ConsoleFrame.grid_propagate(False)
+        self.Console = tkinter.Text(self.ConsoleFrame, width=110, height=31)
+        self.Console.grid(row=1)
+        self.ScrollBar = tkinter.Scrollbar(self.ConsoleFrame, command=self.Console.yview)
+        self.ScrollBar.grid(row=1, column=1, sticky='ns')
 
     def reader_thread(self):
         for line in iter(self.process.stdout.readline, b''):
-            Console.insert(tkinter.END, line)
-            Console.see(tkinter.END)
+            try:
+                self.Console.insert(tkinter.END, line)
+            except:
+                pass
+            try:
+                self.Console.see(tkinter.END)
+            except:
+                pass
         print('done reading')
 
     def stop(self,ende):
@@ -48,27 +63,38 @@ class rcloneProcess:
             ende.destroy()       # wait for the subprocess' exit
         kill_after(countdown=5)
 
-def testthis():
-    for path in execute([r"rclone\rclone-v1.37-windows-amd64\rclone.exe", "ls","secretC:"]):
-        print(path)
-        Console.insert(tkinter.END, path)
 
 def RemoveGUI():
     GUI.destroy()
 
-def ConsoleGUI():
-    ConsoleFrame = tkinter.Frame(ConsoleGUIWin, width=900, height=500)
-    ConsoleFrame.pack(fill="both", expand=True)
-    ConsoleFrame.grid_propagate(False)
-    Console = tkinter.Text(ConsoleFrame, width=110, height=31)
-    Console.grid(row=1)
-    ScrollBar = tkinter.Scrollbar(ConsoleFrame, command=Console.yview)
-    ScrollBar.grid(row=1, column=1, sticky='ns')
-    global Console
 
 def Startapp():
-    app = rcloneProcess(GUI, Console)
-    GUI.protocol("WM_DELETE_WINDOW", app.stop) # exit subprocess if GUI is closed
+    app = rcloneProcess(GUI)
+    GUI.protocol("WM_DELETE_WINDOW", lambda app=app:CloseAndStop(app)) # exit subprocess if GUI is closed
+
+def CloseAndStop(app):
+    app.stop
+    GUI.destroy()
+
+def LoadConfigFromUser(ButtonsFrame, Where=""):
+    global ConfigButtons
+    User = getpass.getuser()
+    for section in ConfigButtons:
+        section.destroy()
+    ConfigButtons = []
+    Config = configparser.ConfigParser()
+    if Where == "":
+        Config.read("C:\\Users\\" + User +"\\.config\\rclone\\rclone.conf")
+    else:
+        Config.read(Where)
+    for sections in Config.sections():
+        ConfigSection = tkinter.Button(ButtonsFrame, text = sections)
+        ConfigSection.grid()
+        ConfigButtons.append(ConfigSection)
+
+def LoadFile():
+    ConfigFile = askopenfilename()
+    return ConfigFile
 
 def ButtonsGUI():
     ButtonsFrame = tkinter.Frame(GUI, width=6, height=6)
@@ -78,9 +104,13 @@ def ButtonsGUI():
     TestCommand.grid(row=0,column=0)
     CloseButton = tkinter.Button(ButtonsFrame, text = "Close", command = lambda: RemoveGUI())
     CloseButton.grid(row=0,column=2)
+    LoadConfig = tkinter.Button(ButtonsFrame, text = "Load User Config", command = lambda: LoadConfigFromUser(ButtonsFrame))
+    LoadConfig.grid()
+    LoadCustomConfig = tkinter.Button(ButtonsFrame, text = "Load Custom Config", command = lambda: LoadConfigFromUser(ButtonsFrame, LoadFile()))
+    LoadCustomConfig.grid()
 
 def MainWindow():
     ButtonsGUI()
-    ConsoleGUI()
     tkinter.mainloop()
+
 MainWindow()
