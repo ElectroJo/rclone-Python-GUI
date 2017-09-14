d77bc8c1ec89447bee37d10c09030138f8cb2c19
ElectroJo <ElectroJo@users.noreply.github.com>
2017-09-14 16:45:54 -0400

    Started the process of adding the ability to use any command.
    Made the close button terminate all rclone processes.
    
    Other various changed

--- script.py
+++ script.py
@@ -7,20 +7,27 @@ import getpass
 from tkinter.filedialog import askopenfilename
 
 GUI = tkinter.Tk()
-GUI.minsize(width=200, height=200)
 ConfigButtons = []
+processlist = []
 
 
 
 #Some code from: https://gist.github.com/zed/42324397516310c86288
 class rcloneProcess:
-    def __init__(self, GUI):
+    def __init__(self, GUI, *Commands):
+        print()
+        self.CommandLists = []
+        self.CommandLists.append(r"rclone\rclone-v1.37-windows-amd64\rclone.exe")
+        for args in Commands:
+            self.CommandLists.append(args)
         self.GUI = GUI
-        self.process = Popen([r"rclone\rclone-v1.37-windows-amd64\rclone.exe", "ls","secretC:"], stdout=PIPE, stderr=STDOUT)
+        self.process = Popen(self.CommandLists, stdout=PIPE, stderr=STDOUT)
+        global processlist
+        processlist.append(self.process)
         t = Thread(target=self.reader_thread, args=[]).start()
         self.EndButton = tkinter.Button(GUI, text="Stop subprocess")
         self.EndButton = tkinter.Button(GUI, text="Stop subprocess", command = lambda: self.stop(self.EndButton))
-        self.EndButton.pack()
+        self.EndButton.grid()
         self.ConsoleGUIWin = tkinter.Toplevel()
         self.ConsoleGUIWin.title("Console")
         self.ConsoleFrame = tkinter.Frame(self.ConsoleGUIWin, width=900, height=500)
@@ -41,10 +48,8 @@ class rcloneProcess:
                 self.Console.see(tkinter.END)
             except:
                 pass
-        print('done reading')
 
     def stop(self,ende):
-        print('stoping')
         self.process.terminate() # tell the subprocess to exit
 
         # kill subprocess if it hasn't exited after a countdown
@@ -52,7 +57,6 @@ class rcloneProcess:
             if self.process.poll() is None: # subprocess hasn't exited yet
                 countdown -= 1
                 if countdown < 0: # do kill
-                    print('killing')
                     self.process.kill() # more likely to kill on *nix
                 else:
                     self.GUI.after(1000, kill_after, countdown)
@@ -65,22 +69,26 @@ class rcloneProcess:
 
 
 def RemoveGUI():
+    for process in processlist:
+        process.terminate()
     GUI.destroy()
-
-
-def Startapp():
-    app = rcloneProcess(GUI)
-    GUI.protocol("WM_DELETE_WINDOW", lambda app=app:CloseAndStop(app)) # exit subprocess if GUI is closed
 
 def CloseAndStop(app):
-    app.stop
+    try:
+        app.stop
+    except:
+        pass
     GUI.destroy()
 
-def LoadConfigFromUser(ButtonsFrame, Where=""):
+def LoadConfigFromUser(ConFigButtonsFrame, Where=""):
+    Columnx = 0
+    Rowy = 1
     global ConfigButtons
     User = getpass.getuser()
     for section in ConfigButtons:
         section.destroy()
+    ConfigLable = tkinter.Label(ConFigButtonsFrame, text = "Remotes", width = 16)
+    ConfigLable.grid()
     ConfigButtons = []
     Config = configparser.ConfigParser()
     if Where == "":
@@ -88,8 +96,14 @@ def LoadConfigFromUser(ButtonsFrame, Where=""):
     else:
         Config.read(Where)
     for sections in Config.sections():
-        ConfigSection = tkinter.Button(ButtonsFrame, text = sections)
-        ConfigSection.grid()
+        ConfigSection = tkinter.Button(ConFigButtonsFrame, width = 16, text = sections)
+        if Columnx == 0:
+            ConfigSection.grid(column=Columnx, row=Rowy)
+            Columnx = 1
+        else:
+            ConfigSection.grid(column=Columnx, row=Rowy)
+            Rowy += 1
+            Columnx = 0
         ConfigButtons.append(ConfigSection)
 
 def LoadFile():
@@ -97,17 +111,27 @@ def LoadFile():
     return ConfigFile
 
 def ButtonsGUI():
-    ButtonsFrame = tkinter.Frame(GUI, width=6, height=6)
-    ButtonsFrame.pack(fill="both", expand=True)
-    ButtonsFrame.grid_propagate(False)
-    TestCommand = tkinter.Button(ButtonsFrame, text = 'Test',command = lambda: Startapp())
+    ButtonsFrame = tkinter.Frame(GUI)
+    ButtonsFrame.grid()
+
+    MainButtons = tkinter.Frame(ButtonsFrame)
+    MainButtons.grid(sticky='w')
+
+    LineCanvas = tkinter.Canvas(ButtonsFrame, height = 1, width = 240)
+    LineCanvas.grid()
+
+    ConFigButtonsFrame = tkinter.Frame(GUI)
+    ConFigButtonsFrame.grid(sticky='w')
+
+    TestCommand = tkinter.Button(MainButtons, text = 'Test', width = 16,command = lambda: rcloneProcess(MainButtons, *CommandList))
     TestCommand.grid(row=0,column=0)
-    CloseButton = tkinter.Button(ButtonsFrame, text = "Close", command = lambda: RemoveGUI())
+    CloseButton = tkinter.Button(MainButtons, text = "Close", width = 16, command = lambda: RemoveGUI())
     CloseButton.grid(row=0,column=2)
-    LoadConfig = tkinter.Button(ButtonsFrame, text = "Load User Config", command = lambda: LoadConfigFromUser(ButtonsFrame))
+    LoadConfig = tkinter.Button(MainButtons, text = "Load User Config", width = 16, command = lambda: LoadConfigFromUser(ConFigButtonsFrame))
     LoadConfig.grid()
-    LoadCustomConfig = tkinter.Button(ButtonsFrame, text = "Load Custom Config", command = lambda: LoadConfigFromUser(ButtonsFrame, LoadFile()))
+    LoadCustomConfig = tkinter.Button(MainButtons, text = "Load Custom Config", width = 16, command = lambda: LoadConfigFromUser(ConFigButtonsFrame, LoadFile()))
     LoadCustomConfig.grid()
+    LineCanvas.create_line(0,0,250,0,fill='black', width=6)
 
 def MainWindow():
     ButtonsGUI()
